3717ddfce3ec3c709f9e2bef931148cf53d38254
GEndr1 <goxhajendri5@gmail.com>
2024-03-19 16:27:10 +0100

    made the size of canvas unaffected by cells no

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const container = document.querySelector(".container");
 const gridSize = prompt("Choose size ")
+const reset = document.querySelector(".reset");
+
+let color = "#000";
+
 
 function grid(){
   for(let i = 0; i < gridSize; i++){
@@ -9,11 +13,19 @@ function grid(){
       const cells = document.createElement('div');
       cells.classList.add("cells");
       row.appendChild(cells);
-      cells.addEventListener('mousedown', ()=>{
-        cells.style.backgroundColor = 'red';
+      cells.addEventListener('mouseover', ()=>{
+        document.body.style.cursor = "pointer";
+        cells.style.backgroundColor = color;
       })
     }
     container.appendChild(row);
   }
 }
 grid();
+reset.addEventListener('click', ()=>{
+  allcells  = document.querySelectorAll(".cells");
+  allcells.forEach(cells => {
+    cells.style.backgroundColor = "white";
+    
+  });
+})
